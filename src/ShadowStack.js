f2213bb6f8ebedc147f7d753056f6efee708fd40
Michael Demarais <m.demarais@gmail.com>
2020-04-21 01:02:48 -0400

    fix missing stylesheet import

--- src/ShadowStack.js
+++ src/ShadowStack.js
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import React, { useCallback } from "react";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import ShadowItem from "./ShadowItem";
 
 const ShadowStack = React.forwardRef(
